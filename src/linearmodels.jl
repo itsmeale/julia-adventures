c657a1d212eee428cb00e954d986263fb7a69187
Ale Farias <0800alefarias@gmail.com>
2022-09-09 16:47:32 -0300

    Removes unecessary plots

--- src/linearmodels.jl
+++ src/linearmodels.jl
@@ -12,14 +12,7 @@ function regression(X, y⃗)
     X = addbias(X)
     X⁻¹ = inv(X' * X) * X'
     w⃗ = X⁻¹ * y⃗
-    ypred = X * w⃗
-
-    mse = (ypred-y⃗).^2
-    scatter(X[:, 1], mse)
-    ylims!(-3, 3)
-
-    scatter(X[:, 1], y⃗)
-    plot!(X[:,  1], ypred, linewidth=4)
+    return w⃗
 end
 
 end
